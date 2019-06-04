7f51a854820be0a1847982834faabd421305104e
Saritha Vijayan <1013422@questgedctvm.com>
2019-06-04 19:28:21 +0530

    message

--- LogAnalyzer/src/main/java/com/quest/loganalyzer/util/ServiceConstants.java
+++ LogAnalyzer/src/main/java/com/quest/loganalyzer/util/ServiceConstants.java
@@ -5,6 +5,6 @@ public class ServiceConstants {
 	public static final String  JDK_PATH = "C:/Program Files/Java/jdk1.8.0_181" ;
 	public static final String  LOGSTASH_NOTSTSARTED_ERROR = "Error!!!!!Logstash server not started" ;
 	public static final String  LOGSTASH_COPYCONFFILE_SUCCESS = "Successfully replaced conf file" ;
-	public static final String  LOGSTASH_COPYCONFFILE_ERROR = "Successfully replaced conf file" ;
+	public static final String  LOGSTASH_COPYCONFFILE_ERROR = "Error while replacing conf file" ;
 	
 }
